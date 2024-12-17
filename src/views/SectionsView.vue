ebcde739a3b071cfe64832635869b07f2e834874
Your Name <sadriddinravshanov19@gmail.com>
2024-12-17 18:39:22 +0500

    bg changed

--- src/views/SectionsView.vue
+++ src/views/SectionsView.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="w-full min-h-[100vh] relative flex justify-center items-center flex-col gap-4 px-3">
-        <img src="../assets/backgrounds/sections.avif" alt="Home" class="absolute w-full h-full brightness-[.4] z-[-1] object-cover">
+        <img src="../assets/backgrounds/menu.jpg" alt="Home" class="absolute w-full h-full z-[-1] object-cover">
         <h2 class="text-white text-[24px] font-bold">Bo'limlar</h2>
         <div class="max-w-[400px] w-full p-5 bg-[#ffffff53] rounded-md flex flex-col gap-3">
             <router-link :to="item?.path" v-for="(item, index) in sections" :key="index" class="flex items-center gap-2 cursor-pointer bg-[#ffffff7b] rounded-md p-2 ease-linear duration-150 border-2 border-transparent hover:border-white">
